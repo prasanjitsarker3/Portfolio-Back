48ab3b83b41abdb42113ef079538e449fd544618
Prasanjit Sarker <sarkerprasanjit379@gmail.com>
2024-07-05 01:53:18 +0600

    Url Set

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,9 @@ import router from "./Route/routes";
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({ origin: "https://dev-murex-seven.vercel.app", credentials: true })
+);
 
 app.use("/dev/v1", router);
 
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,10 @@ import app from "./app";
 
 async function main() {
   try {
-    await mongoose.connect(config.dataBaseUrl as string); //config.dataBaseUrl || "mongodb://127.0.0.1:27017/course-project"
+    await mongoose.connect(
+      "mongodb+srv://devps12Server:zVc72k6GQnPFvKSA@cluster0.rkhkj2w.mongodb.net/devps12Server?retryWrites=true&w=majority&appName=Cluster0"
+    ); //config.dataBaseUrl || "mongodb://127.0.0.1:27017/course-project"
+    console.log(config.dataBaseUrl);
     app.listen(config.port, () => {
       console.log(`Blog Server listening Port On ${config.port}`);
     });
